bb215adb86369220c20420308454fc3673029799
Massimo Santini <massimo.santini@gmail.com>
2016-12-09 10:52:31 +0100

    Better error handling

--- soglie-tre/__main__.py
+++ soglie-tre/__main__.py
@@ -5,15 +5,17 @@ from bs4 import BeautifulSoup
 import requests
 
 r0 = requests.get('http://ac3.tre.it/133/index.jsp?uri=/133/costi-e-soglie.jsp')
-session_id = r0.cookies['JSESSIONID']
+try:
+	session_id = r0.cookies['JSESSIONID']
+except KeyError:
+	exit('soglie_tre: errore di esecuzione, potresti non essere sotto rete Tre')
 r1 = requests.get('http://ac3.tre.it/133/costi-e-soglie.jsp', cookies = {'device': 'mobile-evo', 'JSESSIONID': session_id})
-
 
 soup = BeautifulSoup(r1.text, 'html.parser')
 try:
 	notes = soup.find_all('div', 'box_Note')
 except ValueError:
-	exit('soglie_tre: errore di esecuzione, potresti non essere sotto rete tre')
+	exit('soglie_tre: errore di esecuzione, potresti non essere sotto rete Tre')
 
 def clean(e):
 	return sub( r'<[^>]+>', '', u' '.join(map(lambda _: unicode(_).strip(), e.contents)))
