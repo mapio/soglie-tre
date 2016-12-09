7c9eb5b0974a1c1f64eed11f541deea0c14e6b16
Massimo Santini <massimo.santini@gmail.com>
2016-12-09 10:30:21 +0100

    New

---
+++ README.md
@@ -0,0 +1,4 @@
+Soglie CONSIP
+=============
+
+A command line tool to get usage data for the CONSIP "PA 5 dati" TIM plan.
---
+++ soglie-tre/__main__.py
@@ -0,0 +1,22 @@
+from re import sub
+from sys import exit
+
+from bs4 import BeautifulSoup
+import requests
+
+r0 = requests.get('http://ac3.tre.it/133/index.jsp?uri=/133/costi-e-soglie.jsp')
+session_id = r0.cookies['JSESSIONID']
+r1 = requests.get('http://ac3.tre.it/133/costi-e-soglie.jsp', cookies = {'device': 'mobile-evo', 'JSESSIONID': session_id})
+
+
+soup = BeautifulSoup(r1.text, 'html.parser')
+try:
+	notes = soup.find_all('div', 'box_Note')
+except ValueError:
+	exit('soglie_tre: errore di esecuzione, potresti non essere sotto rete tre')
+
+def clean(e):
+	return sub( r'<[^>]+>', '', u' '.join(map(lambda _: unicode(_).strip(), e.contents)))
+
+for note in notes:
+	print clean(note)
